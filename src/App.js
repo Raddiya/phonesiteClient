fd6f33a69918f474cf8858e7565b3e5ff2a3886b
Raddiya <raddiyasultana.612@gmil.com>
2022-05-06 01:14:57 +0600

    route set

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
+import {Route,Routes} from 'react-router-dom'
 import './App.css';
+import Login from './Components/Auth/Login/Login';
+import RequireAuth from './Components/Auth/RequireAuth/RequireAuth';
+import Signup from './Components/Auth/Signup/Signup';
+import Home from './Components/Home/Home';
+import Footer from './Components/Shared/Footer/Footer';
+import Header from './Components/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello! peramoy life.</h1>
-      <h3>never leave me please :-(</h3>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>} ></Route>
+        <Route path='/login' element={<Login></Login>} ></Route>
+        <Route path='/signup' element={<Signup></Signup>} ></Route>
+        <RequireAuth></RequireAuth>
+        {/* <Route path='/' element={} ></Route>
+        <Route path='/' element={} ></Route> */}
+      
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
